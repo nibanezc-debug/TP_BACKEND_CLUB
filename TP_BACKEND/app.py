b5271132eac2f2fe368f52a6971e66d2d21242a2
SANTIAGO DORTA AROCENA <sdorta@fi.uba.ar>
2026-09-20 20:25:58 -0300

    Register canchas blueprint in app.py

--- TP_BACKEND/app.py
+++ TP_BACKEND/app.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from flask import Flask, request, jsonify
 from src.rutas.deportes import mostrar_deportes_bp
+from src.rutas.canchas import canchas_bp
 from src.repositorios.db import conexion_db
 
 app = Flask(__name__)
 
 app.register_blueprint(mostrar_deportes_bp)
-
+app.register_blueprint(canchas_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) 
